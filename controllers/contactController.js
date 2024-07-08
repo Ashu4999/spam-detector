b54c3d2dc5c6d153196bd9df486a8110443640db
Ashu4999 <ashutoshgorke77@gmail.com>
2024-07-08 18:00:26 +0530

    contact api complete

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,9 +1,89 @@
-const getContact = (req, res) => {
+const { DBModels } = require("../config/dbConn");
+const { Op } = require('sequelize');
+
+const getContact = async (req, res) => {
     try {
-        return res.send("HERE CONTACT");
-    } catch (Exception) {
-        return res.status(500).send({ message: "Something went wrong" });
+        let where = {};
+        const { uuids, names, userIDs } = req.query;
+
+        if (uuids) {
+            where["id"] = { [Op.in]: uuids.split(",") };
+        }
+
+        if (names) {
+            where["contact_name"] = { [Op.in]: names.split(",") };
+        }
+
+        if (userIDs) {
+            where["user_id"] = { [Op.in]: userIDs.split(",") };
+        }
+
+        let contacts = await DBModels.contact.findAll({ where });
+        return res.json(contacts);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Something went wrong" });
     }
 };
 
-module.exports = { getContact };
+const createContact = async (req, res) => {
+    try {
+        const { contact_name, contact_phone_number, user_id } = req.body;
+
+        const newContact = await DBModels.contact.create({
+            contact_name,
+            contact_phone_number,
+            user_id,
+        });
+
+        return res.status(201).json(newContact);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Something went wrong" });
+    }
+};
+
+const updateContact = async (req, res) => {
+    try {
+        const { id } = req.query;
+        const { contact_name, contact_phone_number, user_id } = req.body;
+
+        const contact = await DBModels.contact.findByPk(id);
+
+        if (!contact) {
+            return res.status(404).json({ message: "Contact not found" });
+        }
+
+        await contact.update({
+            contact_name,
+            contact_phone_number,
+            user_id,
+        });
+
+        return res.json(contact);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Something went wrong" });
+    }
+};
+
+const deleteContact = async (req, res) => {
+    try {
+        const { id } = req.query;
+
+        const contact = await DBModels.contact.findByPk(id);
+
+        if (!contact) {
+            return res.status(404).json({ message: "Contact not found" });
+        }
+
+        await contact.destroy();
+
+        return res.json({ message: "Contact deleted successfully" });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Something went wrong" });
+    }
+};
+
+module.exports = { getContact, createContact, updateContact, deleteContact };
--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -1,7 +1,46 @@
 const express = require("express");
 const router = express.Router();
-const { getContact } = require("../controllers/contactController");
+const { getContact, createContact, updateContact, deleteContact } = require("../controllers/contactController");
+const Joi = require("joi");
+const { requestParamsValidate } = require("../middleware/requestParamsValidate");
 
-router.get("/", getContact);
+const getContactSchema = {
+    query: Joi.object({
+        uuids: Joi.string(),
+        names: Joi.string(),
+        userIDs: Joi.string(),
+    }),
+};
+
+const createContactSchema = {
+    body: Joi.object({
+        contact_name: Joi.string().required(),
+        contact_phone_number: Joi.string().required(),
+        user_id: Joi.string(),
+    }),
+};
+
+const updateContactSchema = {
+    query: Joi.object({
+        id: Joi.string().guid({ version: ['uuidv4'] }).required()
+    }),
+    body: Joi.object({
+        contact_name: Joi.string().required(),
+        contact_phone_number: Joi.string().required(),
+        user_id: Joi.string(),
+    }),
+};
+
+const deleteContactSchema = {
+    query: Joi.object({
+        id: Joi.string().guid({ version: ['uuidv4'] }).required()
+    }),
+};
+
+
+router.get("/", requestParamsValidate(getContactSchema), getContact);
+router.post("/", requestParamsValidate(createContactSchema), createContact);
+router.put("/", requestParamsValidate(updateContactSchema), updateContact);
+router.delete("/", requestParamsValidate(deleteContactSchema), deleteContact);
 
 module.exports = router;
